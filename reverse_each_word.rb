a5e0d93331dc4f7c0395b739a99ed9614c33439f
sofia tofigh <sst2140@columbia.edu>
2020-04-05 18:10:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,4 @@
-def reverse_each_word(sentence)
-  array= []
-  sentence=sentence.split(" ")
-  array << sentence
-  new_array=[]
-  array.each do |word|
-    phrase = word.reverse!
-    new_array << phrase
-  end
-  new_array.join(" ")
-  
-end
+
 
 def reverse_each_word_with_each(string)
   # returns that same sentence with each word reversed in place.
