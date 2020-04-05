bfbf2e226644ca273b1e8e482ee6651f22571cd9
sofia tofigh <sst2140@columbia.edu>
2020-04-05 17:51:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,10 @@
 def reverse_each_word(sentence)
+  array= []
+  new_array=[]
+  array << sentence
+  array.each do |word|
+    phrase= word.reverse
+    new_array << phrase
+  end
+  new_array
+end
