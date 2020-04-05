63d9ea2370615d2c4bc5097800672234230a660e
sofia tofigh <sst2140@columbia.edu>
2020-04-05 18:18:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -15,5 +15,9 @@ def reverse_each_word(sentence)
   array=sentence.split(" ")
   new_array=[]
   array.collect do |word|
-    word.reverse
+    phrase = word.reverse
+    new_array << phrase
+  end
+  new_array.join(" ")
+end
     
